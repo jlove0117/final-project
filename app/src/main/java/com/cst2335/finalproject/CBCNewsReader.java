4b3017aa74edd59a9ccd48fd6be56487fbe576e6
Dylan Sherwood <sher0193@algonquinlive.com>
2018-10-31 00:09:02 -0400

    Added empty activities

--- app/src/main/java/com/cst2335/finalproject/CBCNewsReader.java
+++ app/src/main/java/com/cst2335/finalproject/CBCNewsReader.java
@@ -3,11 +3,11 @@ package com.cst2335.finalproject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class CBCNewsReader extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cbcnews_reader);
     }
 }
