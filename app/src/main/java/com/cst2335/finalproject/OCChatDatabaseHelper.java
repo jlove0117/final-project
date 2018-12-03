8bd527d35b62c5928d8fac41d8ae0278bfb9fd97
KaranJoshi97 <josh0024@algonquinlive.com>
2018-12-03 12:46:00 -0500

    OCTranspoBusRoute

---
+++ app/src/main/java/com/cst2335/finalproject/OCChatDatabaseHelper.java
@@ -0,0 +1,40 @@
+package com.cst2335.finalproject;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class OCChatDatabaseHelper /*extends SQLiteOpenHelper*/ {
+
+//    // variables
+//    public static final String DATABASE_NAME = "Messages.db"; // Database name
+//    public static final int VERSION_NUM = 24; // Database version
+//    public static final String TABLE_NAME = "ChatMessagesTable"; // Table name
+//    public static final  String KEY_ID = "_ID"; // Primary key
+//    public static final  String KEY_MESSAGE = "Message"; // Column name
+//
+//
+//    public OCChatDatabaseHelper (Context ctx) {
+//        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+//    }
+//
+//    @Override
+//    // This function is creating the table
+//    public void onCreate(SQLiteDatabase db) {
+//        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + KEY_ID + " Integer PRIMARY KEY AUTOINCREMENT," + KEY_MESSAGE + " text); ");
+//        //db.execSQL(TABLE_NAME);
+//        Log.i("ChatDatabaseHelper", "Calling on Create");
+//    }
+//
+//    @Override
+//    // This function is upgrading the table
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+//        onCreate(db);
+//        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion= " + oldVersion +
+//                " newVersion= " + newVersion);
+//    }
+
+
+}
--- app/src/main/java/com/cst2335/finalproject/OCTranspoBusRouteApp.java
+++ app/src/main/java/com/cst2335/finalproject/OCTranspoBusRouteApp.java
@@ -2,12 +2,27 @@ package com.cst2335.finalproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class OCTranspoBusRouteApp extends Activity {
+
+    protected static final String ACTIVITY_NAME = "OCTranspoBusRouteApp";
+    private String destination;
+    private double latitude;
+    private double longitude;
+    private String startTime;
+    private String gpsSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo_bus_route_app);
+        Log.i(ACTIVITY_NAME, "In onCreate");
+
     }
+
+    Button busButton;
+
+
 }
